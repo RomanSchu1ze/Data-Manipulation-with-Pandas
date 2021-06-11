5a4f7ebb0bb92809e4244b542cedaa06753cab09
Roman Schulze <64545283+RomanSchu1ze@users.noreply.github.com>
2021-06-11 11:52:07 +0200

    Update 5_gemeldete_Stellen.py

--- 5_gemeldete_Stellen.py
+++ 5_gemeldete_Stellen.py
@@ -3,7 +3,6 @@
 
 # ## gemeldete Stellen
 
-# In[32]:
 
 
 # Bibliotheken
@@ -11,21 +10,18 @@ import numpy as np
 import pandas as pd
 
 
-# In[33]:
 
 
 # Daten einlesen
 df = pd.read_excel("generierte Datensätze/Datensatz_komplett.xlsx", index_col=0)
 
 
-# In[34]:
 
 
 # erste fünf Zeilen
 df.head()
 
 
-# In[35]:
 
 
 # definiere technische Berufe
@@ -34,7 +30,6 @@ Technische_Berufe  = df.Berufsgruppe.isin(["24 Metallerzeugung,-bearbeitung, Met
                                 "26 Mechatronik-, Energie- u. Elektroberufe"])
 
 
-# In[36]:
 
 
 # definiere Verkehrsberufe
@@ -42,21 +37,18 @@ Verkehr_Logistikberufe = df.Berufsgruppe.isin(["51 Verkehr, Logistik (außer Fah
                                                   "52 Führer von Fahrzeug- u. Transportgeräten"])
 
 
-# In[37]:
 
 
 # definiere Gastgewerbeberufe
 Gastgewerbeberufe = df.Berufsgruppe.isin(["632 Hotellerie", "633 Gastronomie"])
 
 
-# In[38]:
 
 
 # definiere Elektroberufe
 Elektroberufe = df.Berufsgruppe.isin(["262 Energietechnik", "263 Elektrotechnik"])
 
 
-# In[39]:
 
 
 # Berechne Arbeitslosenquote nach Berufsgruppe und Zeitpunkt
@@ -73,70 +65,60 @@ def get_stellen(data, Berufsgruppe, label):
     return df_stellen
 
 
-# In[40]:
 
 
 # Elektroberufe
 df_elektro = get_stellen(df, Elektroberufe, "Elektroberufe")
 
 
-# In[41]:
 
 
 # technische Berufe
 df_tech = get_stellen(df, Technische_Berufe, "Technische Berufe")
 
 
-# In[42]:
 
 
 # Verkehr und Logistik
 df_verkehr = get_stellen(df, Verkehr_Logistikberufe, "Verkehr -/ Logistikberufe")
 
 
-# In[43]:
 
 
 # Gastgewerberufe
 df_gg = get_stellen(df, Gastgewerbeberufe, "Gastgewerbeberufe")
 
 
-# In[44]:
 
 
 # Zusammenführung
 df = pd.concat([df_elektro, df_tech, df_verkehr, df_gg])
 
 
-# In[45]:
 
 
 # Missings entfernen
 df = df.dropna()
 
 
-# In[46]:
 
 
 # erste fünf Zeilen
 df.head()
 
 
-# In[47]:
 
 
 # Monat
 df["Monat"] = df.Zeitpunkt.str[:3]
 
 
-# In[48]:
 
 
 # Jahr
 df["Jahr"] = df.Zeitpunkt.str[4:8]
 
 
-# In[49]:
 
 
 # kreiere neue Spalte: Monat als Zahl
@@ -146,50 +128,38 @@ df["Monat Zahl"] = df.Monat.replace(
 )
 
 
-# In[50]:
 
 
 # Kreeire neue Datum Spalte
 df["Datum"] = df["Jahr"] + "-" + df["Monat Zahl"] + "-" + "1"
 
 
-# In[51]:
 
 
 # konvertiere Strinf zu Datum
 df["Datum"] = pd.to_datetime(df.Datum, format="%Y-%m-%d")
 
 
-# In[53]:
 
 
 # Filter dataframe
 df = df[["Datum", "DB Berufsgruppe", "gemeldete Stellen"]]
 
 
-# In[54]:
 
 
 # soritere daten nach Berufsgruppe und Datum
 df_sorted = df.sort_values(by=["DB Berufsgruppe", "Datum"])
 
 
-# In[55]:
 
 
 # erste fünf Zeilen
 df_sorted.head()
 
 
-# In[56]:
 
 
 # ALs Excel File speichern
 df_sorted.to_excel("generierte Datensätze/gem_Stellen_Zeitreihe.xlsx") 
-
-
-# In[ ]:
-
-
-
 
