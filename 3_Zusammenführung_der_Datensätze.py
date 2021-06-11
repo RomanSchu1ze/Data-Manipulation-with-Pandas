157fc98b17f9c0535d0afbf1147b1aa8b7e57698
Roman Schulze <64545283+RomanSchu1ze@users.noreply.github.com>
2021-06-11 11:45:55 +0200

    Update 3_Zusammenführung_der_Datensätze.py

--- 3_Zusammenführung_der_Datensätze.py
+++ 3_Zusammenführung_der_Datensätze.py
@@ -3,15 +3,12 @@
 
 # ## Zusammenführung der Beschäftigungs- und Arbeitslosenzahlen in eine einheitliche Datenmatrix
 
-# In[462]:
 
 
 # Bibliotheken
 import numpy as np
 import pandas as pd
 
-
-# In[463]:
 
 
 # laden und vorformatieren Daten 
@@ -35,70 +32,60 @@ df_svb["ID"] = Id
 df_svb.drop(["in Vollzeit", "in Teilzeit"], axis=1, inplace=True)
 
 
-# In[464]:
 
 
 # erste fünf Zeilen Alo
 df_alo.head()
 
 
-# In[465]:
 
 
 # erste fünf Zeilen agB
 df_agb.head()
 
 
-# In[466]:
 
 
 # erste fünf Zeilen svb
 df_svb.head()
 
 
-# In[467]:
 
 
 # Format der SVB Daten, entspricht dem Format der aGB Daten
 df_svb.shape
 
 
-# In[468]:
 
 
 # Format der Alo Daten
 df_alo.shape
 
 
-# In[469]:
 
 
 # Merge von SVB und agB
 result = pd.merge(df_svb, df_agb, how="outer", on=["ID", "Berufsgruppe"], suffixes=["_svB", "_agB"])
 
 
-# In[470]:
 
 
 # erste fünf Zeilen
 result.head()
 
 
-# In[471]:
 
 
 # Merge von alo und Kombination aus svb und agB
 final = pd.merge(result, df_alo, how="outer", on=["ID", "Berufsgruppe"])
 
 
-# In[472]:
 
 
 # erste 5 Zeilen der finalen Datenmatrix
 final.head()
 
 
-# In[474]:
 
 
 # Format des finalen Datensatzes
@@ -107,14 +94,12 @@ final.shape
 
 # ###### Speicherung der Daten als Excel Datei
 
-# In[475]:
 
 
 # ALs Excel File speichern
 final.to_excel("generierte Datensätze/Datensatz_komplett.xlsx") 
 
 
-# In[ ]:
 
 
 
